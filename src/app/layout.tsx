bb907f3adc1b9ad39a9c182100d4a6556282909f
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-09-28 17:14:19 +0530

    Refactor metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Create next and appwrite app",
-  description: "learning it on youtube",
+  title: "Next | Appwrite app",
+  description: "Practiced and developed with Navarasan",
 };
 
 export default function RootLayout({
